d06b64cbcb63b461dfbc8e21ebc8705d9a54c049
Carolina Oldertz <carolina@MacBook-Air.local>
2026-01-16 15:13:19 +0100

    added persist from zustand to make the todo items stay in localstorage

--- src/store/ToDoStore.jsx
+++ src/store/ToDoStore.jsx
@@ -1,8 +1,11 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
-export const useToDoStore = create(set => ({
+export const useToDoStore = create(persist(set => ({
   todos: [],
-  addTodo: (text) => set((state) => ({todos: [...state.todos, {text, completed:false, id: Date.now()}]})),
-  removeTodo: (id) => set((state) => ({todos: state.todos.filter(t => t.id !== id)})),
-  toggleTodo: (id) => set((state) => ({todos: state.todos.map(t => t.id === id ? {...t, completed: !t.completed}: t)}))
+  addTodo: (text) => set((state) => ({ todos: [...state.todos, { text, completed: false, id: Date.now() }] })),
+  removeTodo: (id) => set((state) => ({ todos: state.todos.filter(t => t.id !== id) })),
+  toggleTodo: (id) => set((state) => ({ todos: state.todos.map(t => t.id === id ? { ...t, completed: !t.completed } : t) }))
+}), {
+  name: "todo-storage" // name of the item in the storage (must be unique)
 }))
