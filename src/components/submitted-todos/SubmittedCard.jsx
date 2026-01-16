a52b57182e9f06fcc77446f1dc499549a2c974b6
Carolina Oldertz <carolina@MacBook-Air.local>
2026-01-16 19:38:14 +0100

    added color change to background of todo card when todo is completed

--- src/components/submitted-todos/SubmittedCard.jsx
+++ src/components/submitted-todos/SubmittedCard.jsx
@@ -1,16 +1,18 @@
 import styled from "styled-components"
 import { ToDoCheckbox } from "./ToDoCheckbox"
 import { useToDoStore } from "../../store/ToDoStore"
+import moment from "moment"
 
 
 const StyledCard = styled.div`
   font-size: ${({ theme }) => theme.fontSizes.md};
-  background: ${({ theme }) => theme.colors.secondary};
+  background-color: ${({ $isCompleted, theme }) => ($isCompleted ? "#adadad" : theme.colors.secondary)};
   border: 2px ${({ theme }) => theme.colors.primary} solid;
   box-shadow: ${({ theme }) => theme.shadows.card};
-
+  
+  
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   padding: 10px;
   margin: 15px auto 10px auto;
   width: 85%;
@@ -18,19 +20,31 @@ const StyledCard = styled.div`
   border-radius: 6px;
   justify-content: space-between;
 
-
   @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
     font-size: ${({ theme }) => theme.fontSizes.lg};
     width: 60%;
     
-  }
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
-    width: 45%;
-  }
+    }
+    
+    @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+      width: 45%;
+      }
+      
+      `
+const StyledContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `
+
 const StyledContainer = styled.div`
   display: flex;
+`
+
+const StyledTimestamp = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  margin: 0 6px 0 0;
+  align-self: flex-end;
 `
 
 const StyledText = styled.p`
@@ -53,15 +67,20 @@ const StyledButton = styled.button`
 export const SubmittedCard = ({ todo }) => {
   const { removeTodo, toggleTodo } = useToDoStore()
 
+  const formattedDate = moment(todo.createdAt).fromNow()
+
   return (
-    <StyledCard>
-      <StyledContainer>
-        <ToDoCheckbox checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
-        <StyledText $isCompleted={todo.completed}>{todo.text}</StyledText>
-      </StyledContainer>
-      <StyledButton type="button" onClick={() => removeTodo(todo.id)}>
-        <StyledImage src="icons&images/x.png" alt="Delete to-do" />
-      </StyledButton>
+    <StyledCard $isCompleted={todo.completed}>
+      <StyledContentContainer>
+        <StyledContainer>
+          <ToDoCheckbox checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
+          <StyledText $isCompleted={todo.completed}>{todo.text}</StyledText>
+        </StyledContainer>
+        <StyledButton type="button" onClick={() => removeTodo(todo.id)}>
+          <StyledImage src="icons&images/x.png" alt="Delete to-do" />
+        </StyledButton>
+      </StyledContentContainer>
+      <StyledTimestamp>{formattedDate}</StyledTimestamp>
     </StyledCard>
   )
 }
--- src/store/ToDoStore.jsx
+++ src/store/ToDoStore.jsx
@@ -1,11 +1,13 @@
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
+import moment from "moment"
 
 export const useToDoStore = create(persist(set => ({
   todos: [],
-  addTodo: (text) => set((state) => ({ todos: [...state.todos, { text, completed: false, id: Date.now() }] })),
+  addTodo: (text) => set((state) => ({ todos: [...state.todos, { text, completed: false, id: Date.now(), createdAt: moment().valueOf() }] })),
   removeTodo: (id) => set((state) => ({ todos: state.todos.filter(t => t.id !== id) })),
   toggleTodo: (id) => set((state) => ({ todos: state.todos.map(t => t.id === id ? { ...t, completed: !t.completed } : t) }))
 }), {
-  name: "todo-storage" // name of the item in the storage (must be unique)
+  name: "todo-storage"
+
 }))
